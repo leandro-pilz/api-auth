33b216d7b98305dd05ace8f3c6cce11e09866b6c
Leandro Pilz <leandro.pilz@bem.inf.br>
2021-06-19 08:59:43 -0300

    first commit.

---
+++ src/main/java/com/apiauth/B2bApiAuthApplication.java
@@ -0,0 +1,13 @@
+package com.apiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bApiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2bApiAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiauth/B2bApiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bApiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
